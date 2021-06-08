dc73d00e31863f52ba934ab7599150b86fd7e2d3
AryaPutra99 <85555204+AryaPutra99@users.noreply.github.com>
2021-06-08 17:35:42 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,339 @@
+<html>
+<head>
+  <title>Citra Cantik</title>
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+  <link rel="stylesheet" href="boostrap/js/bootstrap.bundle.js">
+  <link rel="stylesheet" href="bootstrap/js/bootstrap.bundle.min.js">
+  <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+  <style>
+    .bg-pink {
+      background-color:   #FFC0CB;
+    }
+  </style>
+  <style>
+    .border {
+      border-color: black !important;
+    }
+  </style>
+  <style>
+    .border-pink {
+      border-color: pink ;
+    }
+  </style>
+  <style>
+    .a-putih {
+      color: white
+    }
+  </style>
+  <style>
+    .container-footer {
+      margin: 0 auto;
+      width: 760px;
+      max-width: 100%
+    }
+  </style>
+</head>
+<body>
+  <section> 
+    <nav class="container-fluid navbar navbar-expand-sm navbar-secondary bg-pink">
+      <a class="navbar-brand px-5" href="#">
+        <img src="logocitra.jpg" alt="Logo Citra" width="80" height="60">
+      </a>
+      <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+        <ul class="navbar-nav ">
+          <li class="nav-item active ">
+            <a class="nav-link hoverini-produk" href="index.php">
+              <font color="white">
+                Produk
+              </font>
+              <span class="sr-only"></span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link px-5" href="tipsdanartikel.php">
+                <font color="white">
+                  Tips Dan Artikel
+                </font>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link px-5" href="ceritacitra.php">
+                <font color="white">
+                  Cerita Citra
+                </font>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link px-5" href="signup.php">
+                <font color="white">
+                  Sign Up
+                </font>
+              </a>
+            </li>
+            <li>
+             <form class="form-inline my-1 my-md-0 justify-content-end">
+              <input class="form-control justify-content-end" type="text" placeholder="Search" aria-label="Search">
+            </form>
+            <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+              <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+
+                </div>
+              </div>
+            </div>
+          </li>   
+        </ul>
+      </div>
+    </nav>
+  </section>
+  <div class="collapse bg-pink py-4" id="showini-produk">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-2 text-center">
+          <a href="produk.html" class="text-decoration-none">
+            Semua Produk
+          </a>
+        </div>
+        <div class="col-md-2 text-center">
+          <a href="bodylotion.html" class="text-decoration-none">
+            Body Lotion
+          </a>
+        </div>
+        <div class="col-md-2 text-center">
+          <a href="facecare.html" class="text-decoration-none">
+            Face Care
+          </a>
+        </div>
+        <div class="col-md-2 text-center" >
+          <a href="bodywash.html" class="text-decoration-none">
+            Body Wash
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+</section>
+<div>
+  <div>
+    <img src="iklancitra.jpg" class="d-block w-100" alt="...">
+    <div class="d-none d-md-block">
+    </div>
+    <div class="col-md-12">
+      <div class="row">
+        <div class="bg-pink">
+         <br>
+       </div>
+     </div>
+   </div>
+   <div class="row">
+    <div class="col-md-3">
+      <a href="page1.html " >
+       <img src="1.jpg" alt="satu">
+     </a>
+   </div>
+   <div class="col-md-3">
+    <a href="page2.html">
+      <img src="2.jpg" alt="dua">
+    </a>
+  </div>
+  <div class="col-md-3">
+    <a href="page3.html">
+      <img src="3.jpg" alt="tiga" >
+    </a>
+  </div>
+  <div class="col-md-3">
+    <a href="page4.html">
+      <img src="4.jpg" alt="empat">
+    </a>
+  </div>
+</div>
+<div class="col-md-12">
+  <div class="row">
+    <div class="bg-pink">
+     <br>
+   </div>
+ </div>
+</div>
+<div class="row">
+  <div class="col-md-4"> <hr> </div>
+  <div class="col-md-4">
+    <h1>
+      <font color="pink">
+        HIGHLIGHT PRODUCTS
+      </font>
+    </h1>
+  </div>
+  <div class="col-md-4"> <hr> </div>
+</div>
+<section class="py-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <a href="citra1.html">
+          <img src="citra1.jpg" alt="Citra1" width="300" height="300" class="border-pink">
+          <figcaption>
+            <font color="pink">
+              <h4 style="text-align: center;">
+                Citra Body Lotion Natural Glowing White Uv
+              </h4>
+            </font>
+          </figcaption>
+        </a>
+      </div>
+      <div class="col-md-3">
+        <a href="citra2.html">
+          <img src="citra2.jpg" alt="citra2" width="300" height="300" class="border-pink">
+          <figcaption>
+            <font color="pink">
+              <h4 style="text-align: center;">
+                Citra Body Lotion Pearly White Uv
+              </h4>
+            </font>
+          </figcaption>
+        </a>
+      </div>
+      <div class="col-md-3">
+        <a href="citra3.html">
+          <img src="citra3.webp" alt="citra3" width="300" height="300" class="border-pink">
+          <figcaption>
+            <font color="pink">
+              <h4 style="text-align: center;">
+                Citra Sakura Fair Uv Facial Foam
+              </h4>
+            </font>
+          </figcaption>
+        </a>
+      </div>
+      <div class="col-md-3">
+        <a href="citra4.html">
+          <img src="citra4.jpg" alt="citra4" width="300" height="300" class="border-pink">
+          <figcaption>
+            <font color="pink" class="text-decoration-none">
+              <h4 style="text-align: center;">
+                Citra Green Tea Antiacne Moisturizer
+              </h4>
+            </font>
+          </figcaption>
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
+<div class="container-fluid">
+ <footer class=" container-fluid bg-pink">
+  <div class=" py-5">
+   <div class="row">
+    <div class="col-md-1">
+      <a href="unilever.html">
+        <img src="unilever.jpg" alt="Unilever" width="50" height="50">
+      </a>
+    </div>
+    <div class="col-md-5">
+     <h5 class="a-putih">
+      PT UNILEVER INDONESIA <br>
+    </h5>
+    <span class="a-putih">
+      © 2019, Hak cipta oleh Unilever Indonesia, dilindungi undang-undang
+    </span>
+  </div>
+  <div class="col-md-2">
+    <ul class="list-unstyled">
+      <li> 
+        <a href="index.php"  class="a-putih">
+          Home
+        </a>
+      </li>
+      <li>
+        <a href="semuaproduk.php" class="a-putih">
+          Semua Produk
+        </a>
+      </li>
+      <li>
+        <a href="tipsdanartikel.php"  class="a-putih">
+          Tips Dan Artikel
+        </a>
+      </li>
+      <li>
+        <a href="ceritacitra.php"  class="a-putih">
+          Cerita Citra
+
+
+        </a>
+      </li>
+      <li>
+        <a href="faq.php"  class="a-putih">
+          FAQ
+        </a>
+      </li>
+    </ul>
+  </div>
+  <div class="col-md-2">
+    <ul class="list-unstyled">
+      <li>
+        <a href="contactus.php"  class="a-putih">
+          Contact Us
+        </a>
+      </li>
+      <li>
+        <a href="privacynotice.php"  class="a-putih">
+          Privacy Notice
+        </a>
+      </li>
+      <li>
+        <a href="cookienotice.php"  class="a-putih">
+          Cookie Notice
+        </a>
+      </li>
+      <li>
+        <a href="sitemap.php"  class="a-putih">
+          Site Map
+        </a>
+      </li>
+      <li>
+        <a href="signup.php"  class="a-putih">
+          Sign Up
+        </a>
+      </li>
+    </ul>
+  </div>
+  <div class="col-md-2">
+    <ul class="list-unstyled">
+      <li>
+        <i class="fa fa-facebook">
+         <a href="fb.html"  class="a-putih">
+          Cantik Citra
+        </a>
+      </i>
+    </li>
+    <li>
+      <i class="fa fa-twitter">
+        <a href="tw.html"  class="a-putih"> 
+          @Cantik_Citra
+        </a>
+      </i>
+    </li>
+    <li>
+      <i class="fa fa-youtube">
+        <a href="yt.html"  class="a-putih">
+          Cantik Citra
+        </a>
+      </i>
+    </li>
+    <li>
+      <i class="fa fa-instagram">
+        <a href="ig.html"  class="a-putih">
+          Cantik Citra
+        </a>
+      </i>
+    </li>
+  </ul>
+</div>
+</div>
+</div>
+</footer>
+</div>
+</section>
+
+</body>
+
+</html>
